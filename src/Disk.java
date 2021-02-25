67ddc41776de8e5ab439392d2d5bb99aa969fcb0
kbferrigan <kbferrigan@outlook.com>
2021-02-25 16:04:17 -0600

    init

---
+++ src/Disk.java
@@ -0,0 +1,5 @@
+public class Disk {
+    //First, implement a data type Disk.java that represents a 1GB disk, and contains a list of all of the files it is sorting.
+    //This data type should implement the Comparable<Disk> interface so that you can use it with a priority queue.
+
+}
---
+++ src/IntegerSorter.java
@@ -0,0 +1,7 @@
+public class IntegerSorter {
+    //Experienced travelers know that if small items are packed last, they can fit snugly in the odd gaps in nearly filled luggage.
+    //This motivates a smarter strategy:  process the items from biggest to smallest.
+    //The worst-fit decreasing heuristic is to do worst-fit, but first preprocess the file sizes so that they are in descending order.
+    //A modular way to implement this heuristic is to write a separate program IntegerSorter.java that reads in a sequence of integers and prints them out in descending order.
+    //Then you can pipe the results through your worst-fit heuristic.
+}
