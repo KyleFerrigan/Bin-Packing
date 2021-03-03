60316db4ad40843db8cbdf3ee5388179053399e8
pagalati <78822760+pagalati@users.noreply.github.com>
2021-03-03 13:48:08 -0600

    Update WorstFit.java
    added Scanner for the input20.txt file

--- src/WorstFit.java
+++ src/WorstFit.java
@@ -1,7 +1,22 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class WorstFit {
-    public static void main(String args[]){
+    public static void main(String args[]) throws FileNotFoundException{
+        File file = new File("input20.txt");
+        try{
+            Scanner scan = new Scanner(file);
+            while (scan.hasNextLine()){
+                int digit = scan.nextInt();
+                System.out.println(digit);
+            }
+            scan.close();
+        }
+        catch (FileNotFoundException e){
+            System.out.println(e);
+        }
         //TODO Sort biggest to smallest text file
         //TODO Start with single disk: Put biggest files into the disks until all current disks cant fit anymore then make a new disk
-
     }
 }
