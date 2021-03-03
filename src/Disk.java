d897e18ba2c99556d9643669a256f02e4a8da534
kbferrigan <kbferrigan@outlook.com>
2021-03-03 15:43:48 -0600

    fixed Disk.java

--- src/Disk.java
+++ src/Disk.java
@@ -9,16 +9,14 @@ public class Disk implements Comparable<Disk>{
     public Disk(int diskIDIn, int diskSize){ // input the DiskID & the max number of files so we can set the size of filesArray
         diskID = diskIDIn; //set diskID
         sizeRemaining = 1000000; //1GB
-        int[] filesArray = new int[diskSize];
-
+        int[] filesArray = new int[diskSize]; //set to largest size possible given the amount of input nums
     }
 
     @Override
     public int compareTo(Disk that) {
-        if (this.sizeRemaining < that.sizeRemaining) return -1; //if the size remaining is less return -1
+        if (this.sizeRemaining < that.sizeRemaining) return -1; //if the size remaining is less than that return -1
         else if (this.sizeRemaining == that.sizeRemaining) return 0; //if sizes are the same return 0
-        else if (this.sizeRemaining > that.sizeRemaining) return 1; //if the size remaining is less return 1
-        else return -9999999; //error checking
+        else return 1; //if the size remaining is greater than that return 1
     }
 
     public int getDiskID(){//getter method
@@ -27,12 +25,12 @@ public class Disk implements Comparable<Disk>{
 
     public int getSizeRemaining(){//getter method
         return sizeRemaining;
-    }//todo fix this so it actually calculates the current size
+    }
 
     public String getFileSizesString(){ //returns all individual files sizes as string
         String temp = "";
         for (int i=0; i < filesArray.length; i++){
-            temp = temp + filesArray[i] + " ";
+            temp = temp + filesArray[i] + "\n";
         }
         return temp;
     }
@@ -40,5 +38,6 @@ public class Disk implements Comparable<Disk>{
     public void addFile(int fileSizeIn){
         filesArray[curFileLoc] = fileSizeIn;
         curFileLoc++;
+        sizeRemaining = sizeRemaining - fileSizeIn;
     }
 }
