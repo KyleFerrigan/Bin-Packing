780de271ce2c2acd68c4c48056b126dd997f745c
pagalati <78822760+pagalati@users.noreply.github.com>
2021-03-03 15:27:01 -0600

    Update IntegerSorter.java
    re-added sort() function for use in WorstFit.java
    cleaned up some code

--- src/IntegerSorter.java
+++ src/IntegerSorter.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
@@ -9,11 +10,16 @@ import java.util.Scanner;
 //The worst-fit decreasing heuristic is to do worst-fit, but first preprocess the file sizes so that they are in descending order.
 //A modular way to implement this heuristic is to write a separate program IntegerSorter.java that reads in a sequence of integers and prints them out in descending order.
 //Then you can pipe the results through your worst-fit heuristic.
-public class IntegerSorter { //TODO sort incoming integers biggest to smallest
+
+public class IntegerSorter {
     File file = new File("input20.txt");
     ArrayList<Integer> arr = new ArrayList<>();
     
-    public void Integersorter(){
+    public IntegerSorter(){
+        
+    }
+    
+    public void ScanTxtFile() throws IOException {
         try{
             Scanner scan = new Scanner(file);
             while (scan.hasNext()){
@@ -24,7 +30,10 @@ public class IntegerSorter { //TODO sort incoming integers biggest to smallest
         catch (FileNotFoundException e){
             System.out.println(e);
         }
-        Collections.sort(arr, Collections.reverseOrder()); //Sorts the arraylist in descending order
     }
 
+    public ArrayList<Integer> sort(){
+        Collections.sort(arr, Collections.reverseOrder()); //Sorts the arraylist in descending order
+        return this.arr;
+    }
 }
